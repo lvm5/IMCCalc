62d731f6962e4eeafb9cac0290b853cc78a91c9a
Leandro Vansan <59117400+lvm5@users.noreply.github.com>
2025-07-18 19:10:09 -0300

    Initial Commit

---
+++ IMC_Calc/IMC_CalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  IMC_CalcApp.swift
+//  IMC_Calc
+//
+//  Created by Leandro Morais on 2025-07-18.
+//
+
+import SwiftUI
+
+@main
+struct IMC_CalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
